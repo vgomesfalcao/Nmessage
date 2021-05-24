23ef0dc0f29871670531564ed0dc0d91b3cf01c1
Vinicius <vgomesfalcao@gmail.com>
2021-05-24 00:54:13 -0300

    Word bug fix

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 module.exports = (app)=>{
   const home = app.controllers.home
   app.get('/', home.index)
-  app.post('/login',hode.login)
+  app.post('/login',home.login)
   app.get('/sair', home.logout)
 }
