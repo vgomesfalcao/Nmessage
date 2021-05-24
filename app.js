bde1cd5d3e3766bce8643c40e0ee7f9078d9bf61
Vinicius <vgomesfalcao@gmail.com>
2021-05-24 03:31:42 -0300

    New packages added

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express')
-  , load = require('express-load')
-  , path = require('path')
-  , app = express()
+const load = require('express-load')
+const path = require('path')
+const methodOverride = require('method-override')
+const app = express()
+const session = require('express-session')
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
-
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride())
 app.use(express.static(path.join(__dirname, '/public')))
 
+load('models').then('controllers').then('routes').into(app)
 
-load('models')
-  .then('controllers')
-  .then('routes')
-  .into(app)
-
-app.listen(3000, function () {
-  console.log("Nmessage no ar.")
+app.listen(3000, () => {
+  console.log('Nmessage no ar.')
 })
