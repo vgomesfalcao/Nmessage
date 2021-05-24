9599429e3c47a08ace97fd5fb04f0d377a69ed69
Vinicius <vgomesfalcao@gmail.com>
2021-05-24 00:53:57 -0300

    Add login and logout controllers

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,25 @@
-exports.index = function(req, res, next) {
-  res.render('home/index');
+module.exports = function (app) {
+  const Usuario = app.models.usuario;
+
+  const HomeController = {
+    index: function (req, res, next) {
+      res.render("home/index");
+    },
+    login: function (req, res) {
+      const email = req.body.usuario.email;
+      const nome = req.body.usuario.nome;
+      if (email && nome) {
+        const usuario = req.body.usuario;
+        req.session.usuario = usuario;
+        res.redirect("/contatos");
+      } else {
+        res.redirect("/");
+      }
+    },
+    logout: function (req, res) {
+      req.session.destroy();
+      res.redirect("/");
+    },
+  };
+  return HomeController;
 };
